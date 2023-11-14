209541a76cd1f5f4b00671a123877945b8a1b394
Luke Hand <luke.hand@c5alliance.com>
2023-11-14 16:47:16 +0000

    Connection to server timed out

--- database_utils.py
+++ database_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import yaml
 from sqlalchemy import create_engine
-from sqlalchemy import text
+from sqlalchemy import inspect
 import psycopg2
 
 class DatabaseConnector:
@@ -15,22 +15,23 @@ class DatabaseConnector:
         return data_loaded 
 
     def init_db_engine(self, data_loaded):
-        engine = create_engine(f"""
-            postgresql+
-            psycopg2://
-            {data_loaded['RDS_USER']}:
-            {data_loaded['RDS_PASSWORD']}@
-            {data_loaded['RDS_HOST']}:
-            {data_loaded['RDS_PORT']}/
-            {data_loaded['RDS_DATABASE']}""")
+        engine = create_engine(f"postgresql+psycopg2://"
+                               f"{data_loaded['RDS_USER']}:"
+                               f"{data_loaded['RDS_PASSWORD']}@"
+                               f"{data_loaded['RDS_HOST']}:"
+                               f"{data_loaded['RDS_PORT']}/"
+                               f"{data_loaded['RDS_DATABASE']}")
         return engine
 
-    def list_db_tables(self):
-        return engine.table_names()
+    def list_db_tables(self, engine):
+        inspector = inspect(engine)
+        for table_name in inspector.get_table_names():
+            for column in inspector.get_columns(table_name):
+                print("Column: %s" % column['name'])
         
 
 my_instance = DatabaseConnector()
 my_creds = my_instance.read_db_creds()
 my_engine = my_instance.init_db_engine(my_creds)
-my_tables = my_instance.list_db_tables()
+my_tables = my_instance.list_db_tables(my_engine)
 print(my_tables)
