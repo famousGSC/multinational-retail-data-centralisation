fdc3ab28d6d9b3da1ed329189abc2457c8a09398
Luke Hand <luke.hand@c5alliance.com>
2023-11-21 15:13:31 +0000

    Removed extrenuous methods

--- data_extraction.py
+++ data_extraction.py
@@ -11,12 +11,6 @@ class DataExtractor:
             return table_to_read
 
 
-    def extract_to_dataframe(self, engine):
-        with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn: # Using a context manager negates the need to manually close the connection
-            result = conn.execute(text("SELECT * FROM actor"))
-            for row in result:
-                print(row)
-
 
 my_connector = DatabaseConnector()
 
